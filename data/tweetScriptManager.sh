0d9067aa82c75f8d5ef033d984e5962ab9b1cda9
Matt Blessed <matthew.blessed@hope.edu>
2017-10-22 14:36:51 -0500

    getting tweets is more synchronous day-by-day

--- data/tweetScriptManager.sh
+++ data/tweetScriptManager.sh
@@ -8,17 +8,16 @@ i=0
 while [ "$d" != "$d2" ]; do 
   next=$(date -I -d "$d + 1 day")
   echo "spinning up for ${d} - ${next}"
-  echo "python ../GetTweets.py --querysearch \"$search\" --since \"${d}\" --until \"$next\" --output \"${fileNameFriendlySearch}_${d}.csv\" --maxtweets 1000000 > \"logs/${fileNameFriendlySearch}_${d}_log.out\" &"
 
-  python ../GetTweets.py --querysearch "${search}" --since "${d}" --until "${next}" --output "${fileNameFriendlySearch}_${d}.csv" --maxtweets 1000000 > "logs/${fileNameFriendlySearch}_${d}_log.out" &
+  python ../GetTweets.py --querysearch "${search}" --since "${d}" --until "${next}" --output "${fileNameFriendlySearch}_${d}.csv" --maxtweets 1000000 > "logs/${fileNameFriendlySearch}_${d}_log.out"
   
   d=$next
-  i=$((i+1))
-  if [ "$(expr "$i" % 30)" -eq "0" ]; then
-    echo "waiting for processes to finish..."
-    wait
-    echo -e "processes have finished.\n"
-  fi
+  # i=$((i+1))
+  # if [ "$(expr "$i" % 30)" -eq "0" ]; then
+  #   echo "waiting for processes to finish..."
+  #   wait
+  #   echo -e "processes have finished.\n"
+  # fi
 done
 
 wait
